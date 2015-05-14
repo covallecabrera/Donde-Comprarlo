bc425883fe9448a29c50e2205010d8bc9fcfbda5
covallecabrera <franciscoovalle@192.168.0.5>
2015-05-14 02:23:04 -0400

    Busqueda Categoria v0.1

--- DondeComprarlo/gen/cl/pt1/dondecomprarlo/R.java
+++ DondeComprarlo/gen/cl/pt1/dondecomprarlo/R.java
@@ -25,21 +25,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int TextoBuscar=0x7f080001;
-        public static final int action_settings=0x7f080008;
-        public static final int button1=0x7f080000;
+        public static final int TextoBuscar=0x7f080000;
+        public static final int action_settings=0x7f080009;
+        public static final int button1=0x7f080001;
         public static final int button2=0x7f080002;
         public static final int descripcion=0x7f080005;
         public static final int id1=0x7f080003;
         public static final int imagen1=0x7f080007;
         public static final int nombre=0x7f080004;
         public static final int precio=0x7f080006;
+        public static final int spinner1=0x7f080008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int list_item=0x7f030001;
         public static final int list_item_busqueda=0x7f030002;
-        public static final int todos_productos=0x7f030003;
+        public static final int spinner=0x7f030003;
+        public static final int todos_productos=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
