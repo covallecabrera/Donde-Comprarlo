2c32069a8a6bbb072145258debc2e748dd65aae5
Francisco Ovalle <c.ovallecabrera@uandresbello.edu>
2015-08-18 18:30:51 -0300

    V0.9.3
    Actualización pequeñas mejoras

--- DondeComprarlo/src/cl/pt1/dondecomprarlo/servidor.java
+++ DondeComprarlo/src/cl/pt1/dondecomprarlo/servidor.java
@@ -3,8 +3,8 @@ package cl.pt1.dondecomprarlo;
 public class servidor {
 
 	public static String ip(){
-//		String ip = "http://172.20.10.3";
-		String ip = "http://190.153.212.77";
+		String ip = "http://172.20.10.3";
+//		String ip = "http://190.153.212.77";
 //		String ip = "http://192.168.0.6";
 //		String ip = "http://192.168.1.34";
 		return ip;
@@ -12,8 +12,8 @@ public class servidor {
 	
 	public static String ruta(){
 		
-		String ruta = "/bruna_ovalle/";
-//		String ruta = "/donde_comprarlo/";
+//		String ruta = "/bruna_ovalle/";
+		String ruta = "/donde_comprarlo/";
 		
 		return ruta;
 	}
