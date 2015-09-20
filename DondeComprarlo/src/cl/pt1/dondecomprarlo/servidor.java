9b95ddc265e7b554a6384bab595926e7f76ab583
Francisco Ovalle <c.ovallecabrera@uandresbello.edu>
2015-09-19 21:06:55 -0300

    v0.9.5
    Cambiado servidor…

--- DondeComprarlo/src/cl/pt1/dondecomprarlo/servidor.java
+++ DondeComprarlo/src/cl/pt1/dondecomprarlo/servidor.java
@@ -3,10 +3,11 @@ package cl.pt1.dondecomprarlo;
 public class servidor {
 
 	public static String ip(){
-		String ip = "http://172.20.10.2";
+//		String ip = "http://172.20.10.2";
 //		String ip = "http://190.153.212.77";
 //		String ip = "http://192.168.0.2";
 //		String ip = "http://192.168.1.34";
+		String ip = "http://donde-comprarlo.com";
 		return ip;
 	}
 	
